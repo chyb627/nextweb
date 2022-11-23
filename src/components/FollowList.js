8036ba6459d101218966a5010bd7572321b3ccf8
cha <chyb627@naver.com>
2022-11-23 16:11:25 +0900

    swr 사용하기. 프로필 더보기 3개씩 더 가져오기

--- src/components/FollowList.js
+++ src/components/FollowList.js
@@ -1,13 +1,14 @@
-import { Button, Card, List } from 'antd';
-import { StopOutlined } from '@ant-design/icons';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { List, Button, Card } from 'antd';
+import { StopOutlined } from '@ant-design/icons';
 import { useDispatch } from 'react-redux';
+
 import { UNFOLLOW_REQUEST, REMOVE_FOLLOWER_REQUEST } from '../reducers/user';
 
-const FollowList = ({ header, data }) => {
+const FollowList = ({ header, data, onClickMore, loading }) => {
   const dispatch = useDispatch();
-  const onCancle = (id) => () => {
+  const onCancel = (id) => () => {
     if (header === '팔로잉') {
       dispatch({
         type: UNFOLLOW_REQUEST,
@@ -22,20 +23,22 @@ const FollowList = ({ header, data }) => {
 
   return (
     <List
-      style={{ marginBottom: '20px' }}
+      style={{ marginBottom: 20 }}
       grid={{ gutter: 4, xs: 2, md: 3 }}
       size="small"
       header={<div>{header}</div>}
       loadMore={
         <div style={{ textAlign: 'center', margin: '10px 0' }}>
-          <Button>더 보기</Button>
+          <Button onClick={onClickMore} loading={loading}>
+            더 보기
+          </Button>
         </div>
       }
       bordered
       dataSource={data}
       renderItem={(item) => (
-        <List.Item style={{ marginTop: '20px' }}>
-          <Card actions={[<StopOutlined key="stop" onClick={onCancle(item.id)} />]}>
+        <List.Item style={{ marginTop: 20 }}>
+          <Card actions={[<StopOutlined key="stop" onClick={onCancel(item.id)} />]}>
             <Card.Meta description={item.nickname} />
           </Card>
         </List.Item>
@@ -47,6 +50,8 @@ const FollowList = ({ header, data }) => {
 FollowList.propTypes = {
   header: PropTypes.string.isRequired,
   data: PropTypes.array.isRequired,
+  onClickMore: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default FollowList;
